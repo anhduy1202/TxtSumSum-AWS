66bebaf331eea91c7c711d25fde79f6e55d286fe
Daniel Truong <aduy1122@gmail.com>
2025-04-11 01:58:21 -0700

    Initial commit

---
+++ test/ai-summarizer-fullyhacks.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AiSummarizerFullyhacks from '../lib/ai-summarizer-fullyhacks-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ai-summarizer-fullyhacks-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AiSummarizerFullyhacks.AiSummarizerFullyhacksStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
